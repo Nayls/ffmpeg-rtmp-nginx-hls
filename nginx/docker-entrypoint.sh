cb4619a62928ecc3c328c4bb63b7b0def587632c
Mr. Nayls <nayls@bk.ru>
2022-03-30 12:50:48 +0300

    feat: add APP_HOME env and change sed to envsubst

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -2,11 +2,10 @@
 set -Eeo pipefail
 
 if [[ "$1" = 'app' ]]; then
-    sed -i "s~%HLS_SERVER_HOST%~${HLS_SERVER_HOST}~g" /usr/local/nginx/html/live.html
-    sed -i "s~%RTMP_SERVER_HOST%~${RTMP_SERVER_HOST}~g" /usr/local/nginx/html/obs.html
-    sed -i "s~%RTMP_STREAM_KEY%~${RTMP_STREAM_KEY}~g" /usr/local/nginx/html/obs.html
+    envsubst < ${APP_HOME}/html/live.html > ${APP_HOME}/html/live.html.tmp && mv ${APP_HOME}/html/live.html.tmp ${APP_HOME}/html/live.html
+    envsubst < ${APP_HOME}/html/obs.html > ${APP_HOME}/html/obs.html.tmp && mv ${APP_HOME}/html/obs.html.tmp ${APP_HOME}/html/obs.html
 
-    exec /usr/local/nginx/sbin/nginx -g "daemon off;"
+    exec ${APP_HOME}/sbin/nginx -g "daemon off;"
 else
     exec "$@"
 fi
