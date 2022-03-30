80c874abe840f4e76730ea11d4e7789646ca9c08
Mr. Nayls <nayls@bk.ru>
2022-03-30 08:57:29 +0300

    feat: update envs; add favicon; update nginx conf

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -Eeo pipefail
 
 if [[ "$1" = 'app' ]]; then
-    sed -i "s~%RTMP_SERVER_HOST%~${RTMP_SERVER_HOST}~g" /usr/local/nginx/html/index.html
+    sed -i "s~%HLS_SERVER_HOST%~${HLS_SERVER_HOST}~g" /usr/local/nginx/html/live.html
+    sed -i "s~%RTMP_SERVER_HOST%~${RTMP_SERVER_HOST}~g" /usr/local/nginx/html/obs.html
+    sed -i "s~%RTMP_STREAM_KEY%~${RTMP_STREAM_KEY}~g" /usr/local/nginx/html/obs.html
 
     exec /usr/local/nginx/sbin/nginx -g "daemon off;"
 else
